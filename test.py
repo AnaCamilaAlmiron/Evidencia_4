e0e6bc236c758567a710a083ce09b3bc62f1022b
Ana Camila Almiron <almironanacamila@gmail.com>
2024-09-07 19:20:19 -0300

    commit

---
+++ test.py
@@ -0,0 +1,45 @@
+import unittest
+from unittest.mock import patch
+from evidencia_4 import Maquina_hacer_velas
+
+class TestMaquinaHacerVelas(unittest.TestCase):
+    def setUp(self):
+        # Creamos dos instancias para pruebas: una con parametros correctos y otra con parametros incorrectos.
+        self.prueba_correcta = Maquina_hacer_velas(True, "rojo", True)
+        self.prueba_incorrecta = Maquina_hacer_velas(False, "azul", False)
+        
+    
+    def test_derretir_cera_correcto(self):
+        # Caso cuando se puede derretir la cera.
+        resultado = self.prueba_correcta.derretir_cera()
+        self.assertEqual(resultado, "La cera llego a 80 grados, por lo cual se derritio y esta lista para ser vertida en el molde.")
+    
+    def test_derretir_cera_incorrecto(self):
+        # Caso cuando no se puede derretir la cera.
+        resultado = self.prueba_incorrecta.derretir_cera()
+        self.assertEqual(resultado, "La maquina no puede derretir la cera, controlar la temperatura.")
+    
+    def test_insertar_color_correcto(self):
+        # Caso cuando se inserta el color correcto (rojo).
+        resultado = self.prueba_correcta.insertar_color()
+        self.assertEqual(resultado, "El color que has insertado es rojo, por lo cual se puede continuar con el proceso.")
+    
+    def test_insertar_color_incorrecto(self):
+        # Caso cuando se inserta un color incorrecto (no rojo).
+        resultado = self.prueba_incorrecta.insertar_color()
+        self.assertEqual(resultado, "El color que has insertado no es rojo, por lo cual no se puede continuar con el proceso.")
+    
+    def test_verter_cera_correcto(self):
+        # Caso cuando está listo para verter la cera.
+        resultado = self.prueba_correcta.verter_cera_molde()
+        self.assertEqual(resultado, "Esta listo para verter la cera al molde.")
+    
+    def test_verter_cera_incorrecto(self):
+        # Caso cuando no está listo para verter la cera.
+        resultado = self.prueba_incorrecta.verter_cera_molde()
+        self.assertEqual(resultado, "No esta listo para verter la cera al molde, controlar si se encuentra la cera en el tanque a temperatura correcta.")
+        
+        
+
+if __name__ == "__main__":
+    unittest.main()
